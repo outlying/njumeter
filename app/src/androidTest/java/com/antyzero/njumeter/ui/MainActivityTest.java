9cd07db512173959254804de4af53a15a760e5a4
Iwo Polański <outlying@gmail.com>
2014-08-29 11:57:13 +0200

    Added activity test

---
+++ app/src/androidTest/java/com/antyzero/njumeter/ui/MainActivityTest.java
@@ -0,0 +1,10 @@
+package com.antyzero.njumeter.ui;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+}
