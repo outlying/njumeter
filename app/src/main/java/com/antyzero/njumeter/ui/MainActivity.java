ded112634c84e5046657ff93936fbda9d92593ce
Iwo Polański <outlying@gmail.com>
2014-08-29 14:11:38 +0200

    Craslytics

--- app/src/main/java/com/antyzero/njumeter/ui/MainActivity.java
+++ app/src/main/java/com/antyzero/njumeter/ui/MainActivity.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import com.antyzero.njumeter.R;
+import com.crashlytics.android.Crashlytics;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Crashlytics.start(this);
         setContentView(R.layout.activity_main);
     }
 }
