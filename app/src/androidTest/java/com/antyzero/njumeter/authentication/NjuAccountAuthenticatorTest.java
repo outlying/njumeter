93abbf6c1b61861e156f3f911dcf9e9c97c2e54e
outlying <outlying@gmail.com>
2014-08-29 18:18:05 +0200

    Second method implmentation

--- app/src/androidTest/java/com/antyzero/njumeter/authentication/NjuAccountAuthenticatorTest.java
+++ app/src/androidTest/java/com/antyzero/njumeter/authentication/NjuAccountAuthenticatorTest.java
@@ -45,10 +45,18 @@ public class NjuAccountAuthenticatorTest extends ApplicationTestCase<NjuApplicat
 
         Bundle bundle = authenticator.addAccount( null, null, null, null, null );
 
-        // More or less accurate
-        assertThat(
-                bundle.containsKey( AccountManager.KEY_INTENT ) ||
+        validateAuthBundle( bundle );
+    }
+
+    public void testGetAuthTokenIsValid() throws NetworkErrorException {
+        Bundle bundle = authenticator.getAuthToken( null, null, null, null );
+
+        validateAuthBundle( bundle );
+    }
+
+    private void validateAuthBundle( Bundle bundle ) {
+        assertThat( bundle.containsKey( AccountManager.KEY_INTENT ) ||
                 bundle.containsKey( AccountManager.KEY_ACCOUNT_NAME ) ||
-                bundle.containsKey( AccountManager.KEY_ERROR_CODE ) );
+                bundle.containsKey( AccountManager.KEY_ERROR_CODE ) ).isTrue();
     }
 }
--- app/src/main/java/com/antyzero/njumeter/authentication/NjuAccountAuthenticator.java
+++ app/src/main/java/com/antyzero/njumeter/authentication/NjuAccountAuthenticator.java
@@ -8,14 +8,18 @@ import android.accounts.NetworkErrorException;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 
 /**
  *
  */
 public class NjuAccountAuthenticator extends AbstractAccountAuthenticator {
 
+    private final Context context;
+
     public NjuAccountAuthenticator(Context context) {
         super(context);
+        this.context = context;
     }
 
     @Override
@@ -44,7 +48,46 @@ public class NjuAccountAuthenticator extends AbstractAccountAuthenticator {
 
     @Override
     public Bundle getAuthToken(AccountAuthenticatorResponse response, Account account, String authTokenType, Bundle options) throws NetworkErrorException {
-        return null;
+
+        // Extract the username and password from the Account Manager, and ask
+        // the server for an appropriate AuthToken.
+        final AccountManager am = AccountManager.get(context);
+
+        String authToken = am.peekAuthToken(account, authTokenType);
+
+        // Lets give another try to authenticate the user
+        if ( TextUtils.isEmpty( authToken )) {
+            final String password = am.getPassword(account);
+            if (password != null) {
+                // authToken = sServerAuthenticate.userSignIn(account.name, password, authTokenType);
+            }
+        }
+
+        // If we get an authToken - we return it
+        if (!TextUtils.isEmpty(authToken)) {
+            final Bundle result = new Bundle();
+            result.putString(AccountManager.KEY_ACCOUNT_NAME, account.name);
+            result.putString(AccountManager.KEY_ACCOUNT_TYPE, account.type);
+            result.putString(AccountManager.KEY_AUTHTOKEN, authToken);
+            return result;
+        }
+
+        // If we get here, then we couldn't access the user's password - so we
+        // need to re-prompt them for their credentials. We do that by creating
+        // an intent to display our AuthenticatorActivity.
+
+        final Intent intent = new Intent();
+
+        /*final Intent intent = new Intent(context, AuthenticatorActivity.class);
+        intent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE, response);
+        intent.putExtra(AuthenticatorActivity.ARG_ACCOUNT_TYPE, account.type);
+        intent.putExtra(AuthenticatorActivity.ARG_AUTH_TYPE, authTokenType);*/
+
+        final Bundle bundle = new Bundle();
+
+        bundle.putParcelable(AccountManager.KEY_INTENT, intent);
+
+        return bundle;
     }
 
     @Override
