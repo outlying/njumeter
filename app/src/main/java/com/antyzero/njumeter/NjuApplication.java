c94aba961107ff1dfb41b8f21a12185fecae917d
Iwo Polański <outlying@gmail.com>
2014-08-29 14:30:58 +0200

    Use Crashlytics only for release builds

--- app/src/main/java/com/antyzero/njumeter/NjuApplication.java
+++ app/src/main/java/com/antyzero/njumeter/NjuApplication.java
@@ -2,9 +2,19 @@ package com.antyzero.njumeter;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+
 /**
  *
  */
 public class NjuApplication extends Application {
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if(!BuildConfig.DEBUG) {
+            Crashlytics.start(this);
+        }
+    }
 }
--- app/src/main/java/com/antyzero/njumeter/ui/MainActivity.java
+++ app/src/main/java/com/antyzero/njumeter/ui/MainActivity.java
@@ -12,7 +12,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Crashlytics.start(this);
         setContentView(R.layout.activity_main);
     }
 }
