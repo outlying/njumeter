9f53d2b78ffb40e6795c7126ce979506d9f60c3e
outlying <outlying@gmail.com>
2014-08-31 14:04:44 +0200

    Changes

--- app/src/main/java/com/antyzero/njumeter/authentication/NjuAccountAuthenticator.java
+++ app/src/main/java/com/antyzero/njumeter/authentication/NjuAccountAuthenticator.java
@@ -17,58 +17,61 @@ public class NjuAccountAuthenticator extends AbstractAccountAuthenticator {
 
     private final Context context;
 
-    public NjuAccountAuthenticator(Context context) {
-        super(context);
+    public NjuAccountAuthenticator( Context context ) {
+        super( context );
         this.context = context;
     }
 
     @Override
-    public Bundle editProperties(AccountAuthenticatorResponse response, String accountType) {
+    public Bundle editProperties( AccountAuthenticatorResponse response, String accountType ) {
         return null;
     }
 
     @Override
-    public Bundle addAccount(AccountAuthenticatorResponse response, String accountType, String authTokenType, String[] requiredFeatures, Bundle options) throws NetworkErrorException {
+    public Bundle addAccount( AccountAuthenticatorResponse response, String accountType, String authTokenType,
+                              String[] requiredFeatures, Bundle options ) throws NetworkErrorException {
 
         final Intent intent = new Intent();
 
-        intent.putExtra( AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE, response);
+        intent.putExtra( AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE, response );
 
         final Bundle bundle = new Bundle();
 
-        bundle.putParcelable(AccountManager.KEY_INTENT, intent);
+        bundle.putParcelable( AccountManager.KEY_INTENT, intent );
 
         return bundle;
     }
 
     @Override
-    public Bundle confirmCredentials(AccountAuthenticatorResponse response, Account account, Bundle options) throws NetworkErrorException {
+    public Bundle confirmCredentials( AccountAuthenticatorResponse response, Account account,
+                                      Bundle options ) throws NetworkErrorException {
         return null;
     }
 
     @Override
-    public Bundle getAuthToken(AccountAuthenticatorResponse response, Account account, String authTokenType, Bundle options) throws NetworkErrorException {
+    public Bundle getAuthToken( AccountAuthenticatorResponse response, Account account, String authTokenType,
+                                Bundle options ) throws NetworkErrorException {
 
         // Extract the username and password from the Account Manager, and ask
         // the server for an appropriate AuthToken.
-        final AccountManager am = AccountManager.get(context);
+        final AccountManager am = AccountManager.get( context );
 
-        String authToken = am.peekAuthToken(account, authTokenType);
+        String authToken = am.peekAuthToken( account, authTokenType );
 
         // Lets give another try to authenticate the user
-        if ( TextUtils.isEmpty( authToken )) {
-            final String password = am.getPassword(account);
-            if (password != null) {
+        if( TextUtils.isEmpty( authToken ) ) {
+            final String password = am.getPassword( account );
+            if( password != null ) {
                 // authToken = sServerAuthenticate.userSignIn(account.name, password, authTokenType);
             }
         }
 
         // If we get an authToken - we return it
-        if (!TextUtils.isEmpty(authToken)) {
+        if( !TextUtils.isEmpty( authToken ) ) {
             final Bundle result = new Bundle();
-            result.putString(AccountManager.KEY_ACCOUNT_NAME, account.name);
-            result.putString(AccountManager.KEY_ACCOUNT_TYPE, account.type);
-            result.putString(AccountManager.KEY_AUTHTOKEN, authToken);
+            result.putString( AccountManager.KEY_ACCOUNT_NAME, account.name );
+            result.putString( AccountManager.KEY_ACCOUNT_TYPE, account.type );
+            result.putString( AccountManager.KEY_AUTHTOKEN, authToken );
             return result;
         }
 
@@ -85,23 +88,25 @@ public class NjuAccountAuthenticator extends AbstractAccountAuthenticator {
 
         final Bundle bundle = new Bundle();
 
-        bundle.putParcelable(AccountManager.KEY_INTENT, intent);
+        bundle.putParcelable( AccountManager.KEY_INTENT, intent );
 
         return bundle;
     }
 
     @Override
-    public String getAuthTokenLabel(String authTokenType) {
+    public String getAuthTokenLabel( String authTokenType ) {
         return null;
     }
 
     @Override
-    public Bundle updateCredentials(AccountAuthenticatorResponse response, Account account, String authTokenType, Bundle options) throws NetworkErrorException {
+    public Bundle updateCredentials( AccountAuthenticatorResponse response, Account account, String authTokenType,
+                                     Bundle options ) throws NetworkErrorException {
         return null;
     }
 
     @Override
-    public Bundle hasFeatures(AccountAuthenticatorResponse response, Account account, String[] features) throws NetworkErrorException {
+    public Bundle hasFeatures( AccountAuthenticatorResponse response, Account account,
+                               String[] features ) throws NetworkErrorException {
         return null;
     }
 }
